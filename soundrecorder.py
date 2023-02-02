ade3449cbfd14f0ed24fc978a844f91a1b8fa539
AKHILESH CM <90960753+AKHILESHCM20@users.noreply.github.com>
2023-02-02 07:34:45 +0530

    Update soundrecorder.py

--- soundrecorder.py
+++ soundrecorder.py
@@ -1,1 +1,10 @@
+
 import sounddevice
+from scipy.io.wav file import write
+s=44100 
+second=int(input("Enter the time duration in second: "))
+print("Recording....\n")
+record_voice=sounddevice.rec(int(second*fs), samplerate=fs,channels=2)
+sounddevice.wait()
+write("out.wav", fs, record_voice)
+print("Finished...\nPlease Check it...")
